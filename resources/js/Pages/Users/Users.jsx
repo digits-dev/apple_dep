ac102fc14977bcb2eaa76f5f55aeb6797416a2b4
DTCRepo <marvinmosico@digits.ph>
2024-07-01 17:13:24 +0800

    done edit

--- resources/js/Pages/Users/Users.jsx
+++ resources/js/Pages/Users/Users.jsx
@@ -223,6 +223,11 @@ const Users = ({users, options, queryParams}) => {
                            onChange={handleChange} readOnly/>
                 </div>
                 <div>
+                    <label className="font-nunito-sans font-semibold">Privileges</label>
+                    <DropdownSelect defaultSelect="Select a Privilege" name="privilege_id" options={options} value={editForms.privilege_id} onChange={handleChange} />
+                   
+                </div>
+                <div>
                     <label className="font-nunito-sans font-semibold">Password</label>
                     <input type="password" 
                            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
