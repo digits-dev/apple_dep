d86fdf43c06d825e3a09f270455e694359d47c3f
Mark Anthony Aguilar <markanthonyaguilar@digits.ph>
2024-07-01 13:53:23 +0800

    adjust sidebar

--- resources/js/Layouts/layout/AppSidebar.jsx
+++ resources/js/Layouts/layout/AppSidebar.jsx
@@ -1,74 +1,17 @@
 import React, { useEffect, useState, useContext } from "react";
 import { Link, usePage } from "@inertiajs/react";
-import axios from "axios";
 import { NavbarContext } from "../../Context/NavbarContext";
 import SidebarAccordion from "../../Components/Sidebar/SidebarAccordion";
 
-const capitalizeWords = (str) => {
-    const smallWords = [
-        "a",
-        "an",
-        "and",
-        "as",
-        "at",
-        "but",
-        "by",
-        "for",
-        "if",
-        "in",
-        "nor",
-        "of",
-        "on",
-        "or",
-        "so",
-        "the",
-        "to",
-        "up",
-        "yet",
-    ];
-
-    return str
-        .split("_")
-        .map((word, index) => {
-            if (index === 0 && word === word.toUpperCase()) {
-                return word;
-            } else if (!smallWords.includes(word.toLowerCase())) {
-                return (
-                    word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
-                );
-            } else {
-                return word.toLowerCase();
-            }
-        })
-        .join(" ");
-};
-
-const formatActiveSlug = (pathname) => {
-    const segments = pathname.split("/");
-    const lastSegment = segments.pop() || segments.pop();
-    return lastSegment;
-};
-
-const formatSlug = (pathname) => {
-    const segments = pathname.split("/");
-    const lastSegment = segments.pop() || segments.pop();
-    return capitalizeWords(lastSegment);
-};
-
 const AppSidebar = () => {
-    const { auth } = usePage().props;
     const [open, setOpen] = useState(true);
 
-    const { setTitle } = useContext(NavbarContext);
-
-    const handleMenuClick = (newTitle) => {
-        setTitle(newTitle);
-    };
-
-    // console.log(links);
-
     return (
-        <div  className={`${open ? "w-72" : "w-20" } duration-300 p-5 pt-5 h-full relative`}>
+        <div
+            className={`${
+                open ? "w-72" : "w-20"
+            } duration-300 p-5 pt-5 h-full relative`}
+        >
             {/* BUTTON */}
             <div
                 className="bg-login-bg-color absolute cursor-pointer rounded-full -right-3 top-[20px] border-2 border-black p-2 flex items-center justify-center"
@@ -96,68 +39,9 @@ const AppSidebar = () => {
                     <p className="ml-5 font-semibold text-[25px]">DEP</p>
                 </div>
             </div>
-            <ul
-                className={`mt-20 max-h-[500px] overflow-x-hidden ${
-                    open ? "overflow-y-auto" : "overflow-y-hidden"
-                }`}
-            >
-                <Link
-                    href="dashboard"
-                    onClick={() => handleMenuClick("Dashboard")}
-                >
-                    <li
-                        className={`text-white text-sm flex items-center gap-x-4 cursor-pointer px-2 py-3 hover:bg-sidebar-hover-color rounded-[10px] mb-2 ${
-                            formatActiveSlug(window.location.pathname) ===
-                            "dashboard"
-                                ? "active"
-                                : ""
-                        }`}
-                    >
-                        <img
-                            src="/images/navigation/dashboard-icon.png"
-                            className="w-[24px] h-[24px]"
-                        />
-                        <p
-                            className={`font-nunito-sans font-semibold single-line ${
-                                !open && "hidden"
-                            } `}
-                        >
-                            Dashboard
-                        </p>
-                    </li>
-                </Link>
-            </ul>
-            
-            <SidebarAccordion open={open} />
-            
-            {auth.sessions.admin_is_superadmin? 
-            <div className="text-white">
-                <span>Super Admin</span>
-                <ul>
-                    <Link href="privileges"
-                        onClick={() => handleMenuClick("Privileges")}
-                    >
-                        <li className={`text-white text-sm flex items-center gap-x-4 cursor-pointer px-2 py-3 hover:bg-sidebar-hover-color rounded-[10px] mb-2 ${
-                            formatActiveSlug(window.location.pathname) === "privileges" ? "active" : ""}`}
-                        >
-                            <img src="/images/navigation/dashboard-icon.png" className="w-[24px] h-[24px]"/>
-                            <p className={`font-nunito-sans font-semibold single-line ${!open && "hidden"} `}>Privileges</p>
-                        </li>
-                    </Link>
-                    <Link href="users"
-                        onClick={() => handleMenuClick("Users")}
-                    >
-                        <li className={`text-white text-sm flex items-center gap-x-4 cursor-pointer px-2 py-3 hover:bg-sidebar-hover-color rounded-[10px] mb-2 ${
-                            formatActiveSlug(window.location.pathname) === "users" ? "active" : ""}`}
-                        >
-                            <img src="/images/navigation/dashboard-icon.png" className="w-[24px] h-[24px]"/>
-                            <p className={`font-nunito-sans font-semibold single-line ${!open && "hidden"} `}>User Management</p>
-                        </li>
-                    </Link>
-                </ul>
-            </div> : ''
-            }
 
+            {/* SIDEBAR */}
+            <SidebarAccordion open={open} />
         </div>
     );
 };
