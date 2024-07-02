a71100482dde2c5e4ce7df26aec0464df79724fb
joelricmisa <joelricmisa@digits.ph>
2024-07-02 13:18:10 +0800

    update import style

--- resources/js/Components/Table/Buttons/Import.jsx
+++ resources/js/Components/Table/Buttons/Import.jsx
@@ -21,20 +21,26 @@ const Import = ({path}) => {
     <>
     <TableButton onClick={handleShow}>Import</TableButton>
     {show && 
-    <div  className='fixed inset-0 z-[51]'>
+    <div  className='fixed inset-0 z-[51] font-nunito-sans'>
       {/* modal backdrop  */}
       <div onClick={handleShow} className='bg-black/50 h-full w-full'></div>
       {/* modal content  */}
-      <div className='h-2/6 w-2/6 fixed z-[52] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-100 rounded-md px-5 py-5 flex flex-col gap-4'>
+      <div className='h-[230px] w-[500px] fixed z-[52] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-100 rounded-md px-5 py-5 flex flex-col gap-4'>
         <header className='flex items-center justify-between '>
-          <h1 className='text-lg font-bold'>Import</h1>
+          <h1 className='text-xl font-bold'>Import File</h1>
           <a className='underline underline-offset-2 cursor-pointer' href={path}>Download Template</a>
         </header>
 
-        <form className='flex flex-col h-full'  onSubmit={submit}>
+        <form className='flex flex-col h-full gap-2'  onSubmit={submit}>
           <div className='flex-1 flex gap-3 items-center'>
-            <label htmlFor="file" className='text-lg font-medium'>File:</label>
-            <input className='border  border-primary w-full p-1 rounded-md cursor-pointer' id='file' type="file" onChange={e => setData(e.target.files[0])} />
+            <span class="sr-only">Choose import file</span>
+             <input id='file' type="file" onChange={e => setData(e.target.files[0])}  class="block w-full text-sm text-slate-500
+                file:mr-4 file:py-2 file:px-4
+                file:rounded-full file:border-0
+                file:text-sm file:font-semibold
+                file:bg-primary file:text-gray-200
+                border-2 rounded-full border-primary/10 file:cursor-pointer cursor-pointer
+              "/>
           </div>
           {/* Buttons */}
           <div className='h-10 flex gap-4 justify-center items-center text-sm'>
