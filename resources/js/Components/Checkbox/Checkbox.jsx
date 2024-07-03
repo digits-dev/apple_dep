e8163f81ba40b0791e4b83ba6f8a396b530ea080
joelricmisa <joelricmisa@digits.ph>
2024-07-03 16:42:05 +0800

    fix checkbox

--- resources/js/Components/Checkbox/Checkbox.jsx
+++ resources/js/Components/Checkbox/Checkbox.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Checkbox = ({ id, type, name, handleClick, isChecked }) => {
     return (
-        <div class="inline-flex items-center">
+        <div className="inline-flex items-center">
             <label
-                class="relative flex cursor-pointer items-center rounded-full p-3"
-                for="checkbox-8"
+                className="relative flex cursor-pointer items-center rounded-full p-3"
+                htmlFor="checkbox-8"
                 data-ripple-dark="true"
             >
                 <input
@@ -16,19 +16,19 @@ const Checkbox = ({ id, type, name, handleClick, isChecked }) => {
                     checked={isChecked}
                     className="before:content[''] peer relative h-[18px] w-[18px] cursor-pointer appearance-none rounded-md border border-gray-400 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-500 checked:bg-gray-500 checked:before:bg-gray-500 hover:before:opacity-10 outline-none "
                 />
-                <div class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
+                <div className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        class="h-3.5 w-3.5"
+                        className="h-3.5 w-3.5"
                         viewBox="0 0 20 20"
                         fill="currentColor"
                         stroke="currentColor"
-                        stroke-width="1"
+                        strokeWidth="1"
                     >
                         <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                            clip-rule="evenodd"
+                            clipRule="evenodd"
                         ></path>
                     </svg>
                 </div>
