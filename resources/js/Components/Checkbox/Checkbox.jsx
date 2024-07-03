5330d9f3a1a22421b9c3f8e8faec4c2b3a37613f
Mark Anthony Aguilar <markanthonyaguilar@digits.ph>
2024-07-03 16:37:01 +0800

    checkbox component

--- resources/js/Components/Checkbox/Checkbox.jsx
+++ resources/js/Components/Checkbox/Checkbox.jsx
@@ -14,7 +14,7 @@ const Checkbox = ({ id, type, name, handleClick, isChecked }) => {
                     type={type}
                     onChange={handleClick}
                     checked={isChecked}
-                    className="before:content[''] peer relative h-[18px] w-[18px] cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-500 checked:bg-gray-500 checked:before:bg-gray-500 hover:before:opacity-10 outline-none "
+                    className="before:content[''] peer relative h-[18px] w-[18px] cursor-pointer appearance-none rounded-md border border-gray-400 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-500 checked:bg-gray-500 checked:before:bg-gray-500 hover:before:opacity-10 outline-none "
                 />
                 <div class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
                     <svg
