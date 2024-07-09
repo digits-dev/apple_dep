670de5905cf2df4c7f817a07f0f463988173cd5e
Mark Anthony Aguilar <markanthonyaguilar@digits.ph>
2024-07-09 15:08:36 +0800

    update change pass

--- app/Http/Controllers/Users/ChangePasswordController.php
+++ app/Http/Controllers/Users/ChangePasswordController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Users;
 
+use app\Helpers\CommonHelpers;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Inertia\Inertia;
 
 class ChangePasswordController extends Controller
@@ -12,12 +15,27 @@ class ChangePasswordController extends Controller
     public function getIndex()
     {
         $data = [];
-        
+
         return Inertia::render('Users/ChangePassword');
     }
 
     public function postChangePassword(Request $request) {
-        dd($request);
+
+        $request->validate([
+            'current_password' => 'required',
+            'new_password' => 'required|min:5|confirmed',
+        ]);
+
+        $user = User::find(CommonHelpers::myId());
+
+        if (!Hash::check($request->current_password, $user->password)) {
+            return response()->json(['errors' => ['current_password' => ['The current password is incorrect.']]], 422);
+        }
+
+        $user->password = Hash::make($request->new_password);
+        $user->save();
+
+        return response()->json(['message' => 'Password changed successfully.']);
         
     }
 }
--- resources/js/Pages/Users/ChangePassword.jsx
+++ resources/js/Pages/Users/ChangePassword.jsx
@@ -1,17 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import AppContent from "../../Layouts/layout/AppContent";
 import ContentPanel from "../../Components/Table/ContentPanel";
 import { Head } from "@inertiajs/react";
 import InputWithLogo from "../../Components/Forms/InputWithLogo";
 import TableButton from "../../Components/Table/Buttons/TableButton";
+import DissapearingToast from "../../Components/Toast/DissapearingToast";
 
 const ChangePassword = () => {
+    const [data, setData] = useState({
+        current_password: "",
+        new_password: "",
+        confirmation_password: "",
+    });
+    const [errors, setErrors] = useState({});
+
+    const [loading, setLoading] = useState(false);
+    const [formMessage, setFormMessage] = useState("");
+    const [messageType, setMessageType] = useState("");
+
+    function handleChange(e) {
+        const key = e.target.name;
+        const value = e.target.value;
+        setData((changePasswordData) => ({
+            ...changePasswordData,
+            [key]: value,
+        }));
+        setErrors((prevErrors) => ({ ...prevErrors, [key]: "" }));
+    }
+
+    const validate = () => {
+        const newErrors = {};
+        if (!data.current_password)
+            newErrors.current_password = "Current Password is required";
+        if (!data.new_password)
+            newErrors.new_password = "New Password is required";
+        if (!data.confirmation_password)
+            newErrors.confirmation_password = "Confirm Password is required";
+        return newErrors;
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const newErrors = validate();
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+        } else {
+            setLoading(true);
+            try {
+                const response = await axios.post("/postChangePassword", data, {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                });
+                if (response.data.type == "success") {
+                    setFormMessage(response.data.message);
+                    setMessageType(response.data.type);
+                    setTimeout(() => setFormMessage(""), 3000);
+                    setShowCreateModal(false);
+                    router.reload({ only: ["users"] });
+                } else {
+                    setErrorMessage(response.data.message);
+                }
+            } catch (error) {
+                if (error.response && error.response.status === 422) {
+                    setErrors(error.response.data.errors);
+                } else {
+                    setErrors({
+                        general: "An error occurred. Please try again.",
+                    });
+                }
+            } finally {
+                setLoading(false);
+            }
+        }
+    };
+
     return (
         <>
             <Head title="Change Password" />
             <AppContent>
+                <DissapearingToast type={messageType} message={formMessage} />
                 <ContentPanel>
-                    <div className="flex justify-center my-16 font-nunito-sans gap-x-16 gap-y-5 items-center flex-wrap m-5">
+                    <form
+                        onSubmit={handleSubmit}
+                        className="flex justify-center my-16 font-nunito-sans gap-x-16 gap-y-5 items-center flex-wrap m-5"
+                    >
                         <img
                             src="images/others/changepass-image.png"
                             className="w-80"
@@ -29,13 +102,20 @@ const ChangePassword = () => {
                                 placeholder="Enter Current Password"
                                 type="password"
                                 marginBottom={3}
+                                onChange={handleChange}
                             />
+                            {errors.current_password && (
+                                <div className="font-nunito-sans font-bold text-red-600">
+                                    {errors.current_password}
+                                </div>
+                            )}
                             <InputWithLogo
                                 label="New Password"
                                 logo="images/login-page/password-icon.png"
                                 placeholder="Enter New Password"
                                 type="password"
                                 marginBottom={3}
+                                onChange={handleChange}
                             />
                             <InputWithLogo
                                 label="Confirm Password"
@@ -43,14 +123,17 @@ const ChangePassword = () => {
                                 placeholder="Confirm New Password"
                                 type="password"
                                 marginBottom={8}
+                                onChange={handleChange}
                             />
 
                             <div className="flex justify-between">
                                 <TableButton>Cancel</TableButton>
-                                <TableButton>Save Changes</TableButton>
+                                <TableButton type="submit">
+                                    {loading ? "Saving..." : "Save Changes"}
+                                </TableButton>
                             </div>
                         </div>
-                    </div>
+                    </form>
                 </ContentPanel>
             </AppContent>
         </>
