4af376540d11171a6687ae0def8c598690193732
jaymar018 <jaymarcatapang@digits.ph>
2024-07-15 15:48:30 +0800

    added network time out validation

--- app/Services/AppleDeviceEnrollmentService.php
+++ app/Services/AppleDeviceEnrollmentService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Client\RequestException;
@@ -12,6 +13,7 @@ class AppleDeviceEnrollmentService
     protected $bulkEnrollEndpoint;
     protected $showOrderDetailsEndpoint;
     protected $checkTransactionStatusEndpoint;
+    protected $timeout;
 
     public function __construct()
     {
@@ -19,6 +21,7 @@ class AppleDeviceEnrollmentService
         $this->bulkEnrollEndpoint = config('services.apple_api.bulk_enroll_endpoint');
         $this->checkTransactionStatusEndpoint = config('services.apple_api.check_transaction_status_endpoint');
         $this->showOrderDetailsEndpoint = config('services.apple_api.show_order_details_endpoint');
+        $this->timeout = 15;
     }
 
     public function enrollDevices(array $payload)
@@ -31,6 +34,11 @@ class AppleDeviceEnrollmentService
         return $this->sendRequest($payload, 'bulk un-enroll');
     }
 
+    public function overrideOrder(array $payload)
+    {
+        return $this->sendRequest($payload, 'override order');
+    }
+
     public function checkTransactionStatus(array $requestData)
     {
         $url = $this->baseUrl . $this->checkTransactionStatusEndpoint;
@@ -40,20 +48,21 @@ class AppleDeviceEnrollmentService
                 'Content-Type' => 'application/json',
                 'Accept-Encoding' => '',
             ])
+                ->timeout($this->timeout)
                 ->post($url, $requestData);
 
             if ($response->successful()) {
                 return $response->json();
             }
 
-            Log::error('Apple Device Enrollment API check transaction status request failed', [
-                'message' => $response->body()
-            ]);
 
-            throw new \Exception('Failed to check transaction status');
+            $this->handleErrorResponse('check transaction status', $response);
+        } catch(ConnectionException $e){
+            $this->handleTimeoutException('check transaction status', $e);
+        } catch (RequestException $e) {
+            $this->handleRequestException('check transaction status', $e);
         } catch (\Exception $e) {
-            Log::error('Apple Device Enrollment API exception', ['exception' => $e->getMessage()]);
-            throw $e;
+            $this->handleGeneralException('check transaction status', $e);
         }
     }
 
@@ -66,21 +75,25 @@ class AppleDeviceEnrollmentService
             'orderNumbers' => $orderNumbers,
         ];
 
-        $response = Http::withHeaders([
-            'Content-Type' => 'application/json',
-            'Accept-Encoding' => '',
-        ])
-            ->post($url, $payload);
-
-        if ($response->successful()) {
-            return $response->json();
+        try{
+            $response = Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'Accept-Encoding' => '',
+            ])
+                ->timeout($this->timeout)
+                ->post($url, $payload);
+    
+            if ($response->successful()) {
+                return $response->json();
+            }
+            $this->handleErrorResponse('show order details', $response);
+        } catch(ConnectionException $e){
+            $this->handleTimeoutException('show order details', $e);
+        } catch (RequestException $e) {
+            $this->handleRequestException('show order details', $e);
+        } catch (\Exception $e) {
+            $this->handleGeneralException('show order details', $e);
         }
-
-        Log::error('Apple Device Enrollment API show order details request failed', [
-            'message' => $response->body()
-        ]);
-
-        throw new \Exception('Failed to show order details');
     }
 
     private function sendRequest(array $payload, string $action)
@@ -96,13 +109,17 @@ class AppleDeviceEnrollmentService
             $response = Http::withHeaders([
                 'Content-Type' => 'application/json',
                 'Accept-Encoding' => '',
-            ])->post($url, $payload);
+            ])
+            ->timeout($this->timeout)
+            ->post($url, $payload);
 
             if ($response->successful()) {
                 return $response->json();
             }
 
             $this->handleErrorResponse($action, $response);
+        } catch (ConnectionException $e) {
+            $this->handleTimeoutException($action, $e);
         } catch (RequestException $e) {
             $this->handleRequestException($action, $e);
         } catch (\Exception $e) {
@@ -119,6 +136,13 @@ class AppleDeviceEnrollmentService
         throw new \Exception($errorMessage);
     }
 
+    private function handleTimeoutException(string $action, ConnectionException $e)
+    {
+        $errorMessage = "Apple Device Enrollment API timeout during $action: " . $e->getMessage();
+        Log::error($errorMessage);
+        throw new \Exception($errorMessage);
+    }
+
     private function handleRequestException(string $action, RequestException $e)
     {
         $errorMessage = "Apple Device Enrollment API exception during $action: " . $e->getMessage();
@@ -132,5 +156,7 @@ class AppleDeviceEnrollmentService
         Log::error($errorMessage);
         throw new \Exception($errorMessage);
     }
+
+    
 
 }
