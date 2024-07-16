2daeb54f4d934c6cff80ca8646bbedfcf94dd587
Mark Anthony Aguilar <markanthonyaguilar@digits.ph>
2024-07-16 13:51:25 +0800

    adjust overview data dashboard

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -6,6 +6,7 @@ use App\Helpers\CommonHelpers;
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
 use App\Models\Device;
+use App\Models\EnrollmentList;
 use App\Models\Order;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
@@ -19,19 +20,23 @@ class DashboardController extends Controller
         $data = [];
         $data['customer_count'] = Customer::count();
         $data['orders_count'] = Order::count();
-        $data['devices'] = Device::count();
+        $data['dep_devices_count'] = EnrollmentList::join('orders', 'orders.sales_order_no', '=', 'enrollment_lists.sales_order_no')
+        ->join('list_of_order_lines', 'list_of_order_lines.serial_number', '=', 'enrollment_lists.serial_number')
+        ->where('enrollment_lists.enrollment_status', 3)
+        ->count();
         $data['orders_count_wdate'] = Order::select(DB::raw('DATE(order_date) as date'), DB::raw('count(*) as count'))
                     ->groupBy('date')
                     ->orderBy('date', 'desc')
                     ->get();
 
         $sidebarMenus = CommonHelpers::sidebarMenu();
+
         
         return Inertia::render('Dashboard/Dashboard', [
             'menus' => $sidebarMenus,
             'customer' => $data['customer_count'],
             'orders' => $data['orders_count'],
-            'devices' => $data['devices'],
+            'devices' => $data['dep_devices_count'],
             'orders_count_wdate' => $data['orders_count_wdate'],
         ]);
     }
